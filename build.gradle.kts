a0a4645aa4d632ad354e9e7e365088c05ed5963a
Ben Chatelain <ben@octop.ad>
2023-05-29 13:38:10 -0600

    ➕ junit (5.9.3)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
+val junit_version: String by project
 
 plugins {
     kotlin("jvm") version "1.8.21"
@@ -26,4 +27,6 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$junit_version")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:$junit_version")
 }
