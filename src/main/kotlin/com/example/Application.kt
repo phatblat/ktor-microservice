a89c860899ecd09544902839ff9386061cc9baf6
Ben Chatelain <ben@octop.ad>
2023-05-29 18:30:05 -0600

    👔 Add basic OK response

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -1,13 +1,21 @@
 package com.example
 
+import com.example.plugins.configureRouting
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import com.example.plugins.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
-        .start(wait = true)
+    embeddedServer(Netty, port = 8080) { //}), host = "0.0.0.0", module = Application::module)
+        routing {
+            get("/") {
+                call.respondText("OK")
+            }
+        }
+    }.start(wait = true)
+    println("open http://localhost:8080")
 }
 
 fun Application.module() {
