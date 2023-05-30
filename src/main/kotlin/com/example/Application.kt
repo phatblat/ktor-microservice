eb3f7b38338e091fe5050f7cbc3b38628a80e02f
Ben Chatelain <ben@octop.ad>
2023-05-30 17:36:25 -0600

    🔀 Switch ktor server to CIO

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -3,12 +3,12 @@ package com.example
 import com.example.plugins.configureRouting
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
-import io.ktor.server.netty.*
+import io.ktor.server.cio.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun main() {
-    embeddedServer(Netty, port = 8080) { //}), host = "0.0.0.0", module = Application::module)
+    embeddedServer(CIO, port = 8080) { //}), host = "0.0.0.0", module = Application::module)
         routing {
             get("/") {
                 call.respondText("OK")
